8d2a7adec290d06140c1f917e1c59b65fa62e1dc
Geovani Perez França <geovanipfranca@gmail.com>
2026-01-10 13:44:45 -0300

    feat: add lint and format scripts with ESLint + Prettier

--- src/utils/stringToDate.ts
+++ src/utils/stringToDate.ts
@@ -1,7 +1,7 @@
 export default function stringToDate(text: string): Date {
-  const [data, tempo] = text.split(' ');
-  const [dia, mes, ano] = data.split('/').map(Number);
-  const [hora, minuto] = tempo.split(':').map(Number);
+  const [data, tempo] = text.split(" ");
+  const [dia, mes, ano] = data.split("/").map(Number);
+  const [hora, minuto] = tempo.split(":").map(Number);
   const dataNormalizada = new Date(ano, mes - 1, dia, hora, minuto);
   return dataNormalizada;
 }
